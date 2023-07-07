13d55fd015551979215ced453031dbb911084bb0
unknown <anderson.minetto@unmsm.edu.pe>
2023-07-07 01:08:48 -0500

    Actualizado

--- models/usuario.py
+++ models/usuario.py
@@ -2,16 +2,13 @@ from utils.db import db
     
 class Usuario(db.Model):
     id_usuario = db.Column(db.Integer, primary_key=True)
-    id_rol_usuario = db.Column(db.Integer, db.ForeignKey('rol_usuario.id_rol_usuario'))
-    nombre_usuario = db.Column(db.String(40))
-    email = db.Column(db.String(60))
-    password = db.Column(db.String(20))
+    user_name = db.Column(db.String(300))
+    password = db.Column("password",db.String(50)) #usamos el alias 'pass' para que los reconozca la BD
 
-    def __init__(self, id_usuario, nombre_usuario=None, email=None, password=None, id_rol_usuario=None) -> None:
+    def __init__(self, id_usuario, user_name=None, password=None) -> None:
         self.id_usuario = id_usuario
-        self.nombre_usuario = nombre_usuario
-        self.email = email
+        self.user_name = user_name
         self.password = password
-        self.id_rol_usuario = id_rol_usuario
+        
 
     
--- routes/vista.py
+++ routes/vista.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template as rt, flash, redirect, url_for, request, jsonify
+from flask import Blueprint, jsonify
 
-from models.rol_usuario import Rol_Usuario
 from models.usuario import Usuario
+from models.modulo import Modulo
+from models.rol import Rol
+from models.usuarioRol import Usuario_Rol
+from models.usuarioModulo import Usuario_Modulo
 
 from utils.db import db
 from utils.json import model_to_dict
@@ -10,35 +13,32 @@ bp = Blueprint('vista',__name__, url_prefix="/vista")
 
 @bp.route('/')
 def vista():
-    rol_usuario = Rol_Usuario.query.all()
     usuario = Usuario.query.all()
+    usuario_rol = Usuario_Rol.query.all()
+    modulo = Modulo.query.all()
 
-    data_rol_usuario = []
+    data_usuario_rol = []
     data_usuario = []
+    data_modulo = []
 
-    for obj in rol_usuario:
+    for obj in usuario_rol:
         x = model_to_dict(obj)
-        data_rol_usuario.append(x)
+        data_usuario_rol.append(x)
+
     for obj in usuario:
         x = model_to_dict(obj)
         data_usuario.append(x)
 
+    for obj in modulo:
+        x = model_to_dict(obj)
+        data_modulo.append(x)    
+
     result = {
         'Usuario':data_usuario,
-        'Rol_Usuario':data_usuario
+        'Rol_Usuario':data_usuario_rol,
+        'Modulo':data_modulo
     }
 
-    return jsonify(result)
-
-@bp.route('/rol_usuario')
-def vista_rol_usuario():
-    query = Rol_Usuario.query.all()
-    data = []
-    for obj in query:
-        data.append(model_to_dict(obj))
-    result = {
-        'Rol_Usuario': data
-    }
     return jsonify(result)
 
 @bp.route('/usuario')
@@ -49,5 +49,49 @@ def vista_usuario():
         data.append(model_to_dict(obj))
     result = {
         'Usuario': data
+    }
+    return jsonify(result)
+
+@bp.route('/modulo')
+def vista_modulo():
+    query = Modulo.query.all()
+    data = []
+    for obj in query:
+        data.append(model_to_dict(obj))
+    result = {
+        'Modulo': data
+    }
+    return jsonify(result)
+
+@bp.route('/rol')
+def vista_rol():
+    query = Rol.query.all()
+    data = []
+    for obj in query:
+        data.append(model_to_dict(obj))
+    result = {
+        'Rol': data
+    }
+    return jsonify(result)
+
+@bp.route('/usuario_modulo')
+def vista_usuario_modulo():
+    query = Usuario_Modulo.query.all()
+    data = []
+    for obj in query:
+        data.append(model_to_dict(obj))
+    result = {
+        'Usuario_Modulo': data
+    }
+    return jsonify(result)
+
+@bp.route('/usuario_rol')
+def vista_usuario_rol():
+    query = Usuario_Rol.query.all()
+    data = []
+    for obj in query:
+        data.append(model_to_dict(obj))
+    result = {
+        'Usuario_Rol': data
     }
     return jsonify(result)
