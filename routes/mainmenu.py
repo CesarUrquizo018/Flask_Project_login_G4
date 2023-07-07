3cbd54e7ba6fab649d4364b244a7bd184b3d9601
CesarUrquizo018 <cesar.urquizo@unmsm.edu.pe>
2023-07-07 02:19:50 -0500

    main

--- routes/mainmenu.py
+++ routes/mainmenu.py
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 from flask import session, Blueprint, render_template as rt, request, url_for, redirect
-=======
 from flask import Blueprint, render_template as rt
->>>>>>> c86f0b439ec6531db697860263277189ddd3c334
 from utils.auth import login_required
 
 bp = Blueprint('mainmenu', __name__, url_prefix="/mainmenu")
 
-<<<<<<< HEAD
 @bp.route('/', methods=['GET', 'POST'])
 @login_required
 def mainmenu():
@@ -15,9 +11,7 @@ def mainmenu():
                 session.clear()
                 return redirect(url_for('index.index'))
 
-=======
 @bp.route('/')
 @login_required
 def mainmenu():
->>>>>>> c86f0b439ec6531db697860263277189ddd3c334
     return rt("mainmenu.html")
